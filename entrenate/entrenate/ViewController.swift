257a137353bfefca0aebe2e6b72a49aed37b4209
nadia.co <ngarciaorozco@outlook.com>
2021-03-18 18:15:48 -0600

    Creation of app project

---
+++ entrenate/entrenate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  entrenate
+//
+//  Created by Nadia Garcia on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

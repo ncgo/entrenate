2dce0ca5089fd6d6c0675667aa9ea60d05e05685
nadia.co <ngarciaorozco@outlook.com>
2021-05-28 14:21:44 -0600

    App Rework from scratch

---
+++ entrenate v2/entrenate v2/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  entrenate v2
+//
+//  Created by Nadia Garcia on 28/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

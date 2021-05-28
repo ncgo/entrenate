199a7952fcc17832968efaa7763334e988abfae4
Mauricio Ruffo <mauroruffo16@gmail.com>
2021-05-27 23:32:25 -0700

    Division Storyboards

--- entrenate/entrenate/LogIn y Registro/ViewControllerLogin.swift
+++ entrenate/entrenate/LogIn y Registro/ViewControllerLogin.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class ViewControllerLogin: UIViewController {
+    
+    struct User : Codable {
+        var name: String!
+        var town: String!
+        var grade: String!
+        var email: String!
+        var password: String!
+        var highScore: Int!
+    }
 
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
@@ -16,8 +25,21 @@ class ViewControllerLogin: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
+        view.addGestureRecognizer(tap)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @IBAction func quitaTeclado() {
+        view.endEditing(true)
     }
     
     @IBAction func unwindToFirstViewController(_ sender: UIStoryboardSegue) {
@@ -32,7 +54,7 @@ class ViewControllerLogin: UIViewController {
     }
     
     func isValidPassword(_ password: String) -> Bool {
-        if tfPassword.text! == "" {
+        if tfPassword.text! == ""{
             return false
         }
         return true
@@ -40,6 +62,41 @@ class ViewControllerLogin: UIViewController {
     
     @IBAction func loadUserDefaults(_ sender: Any) {
         let defaults = UserDefaults.standard
+        guard let userInfo = defaults.object(forKey: "user") as? Data else {
+            return
+        }
+        guard let user = try? PropertyListDecoder().decode(User.self, from: userInfo) else {
+            return
+        }
+        print("Username is \(String(describing: user.name))")
+    }
+    
+    
+    @IBAction func btIniciaSesion(_ sender: Any) {
+        if isValidEmail(tfEmail.text!) == true && isValidPassword(tfPassword.text!){
+            
+        }
+        else {
+            let alerta = UIAlertController(title: "Error", message: "Los campos deben estar llenos y el formato de correo debe ser correcta", preferredStyle: .alert)
+            let accion = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alerta.addAction(accion)
+            present(alerta, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func tecladoCambia(notification: Notification) {
+        print("Teclado aparece: \(notification.name.rawValue)")
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height
+        }
+        else {
+            view.frame.origin.y = 0
+        }
+        
     }
     
     /*
--- entrenate/entrenate/LogIn y Registro/ViewControllerRegistro.swift
+++ entrenate/entrenate/LogIn y Registro/ViewControllerRegistro.swift
@@ -27,8 +27,36 @@ class ViewControllerRegistro: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
+        view.addGestureRecognizer(tap)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @IBAction func quitaTeclado() {
+        view.endEditing(true)
+    }
+    
+    @objc func tecladoCambia(notification: Notification) {
+        print("Teclado aparece: \(notification.name.rawValue)")
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height
+        }
+        else {
+            view.frame.origin.y = 0
+        }
+        
     }
     
     @IBAction func saveUserDefaults(_ sender: Any){
@@ -36,6 +64,16 @@ class ViewControllerRegistro: UIViewController {
         let defaults = UserDefaults.standard
         defaults.set(try? PropertyListEncoder().encode(usuario), forKey: "usuario")
         print("Usuario Guardado")
+    }
+    
+    @IBAction func loadUserDefaults(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        guard let userInfo = defaults.object(forKey: "user") as? Data else {
+            return
+        }
+        guard let user = try? PropertyListDecoder().decode(User.self, from: userInfo) else {
+            return
+        }
     }
     
     @IBAction func GuardarInfo(_ sender: Any) {

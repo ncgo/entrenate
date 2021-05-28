9b93ca37428a4ff87684b2928988769fa7f050de
nadia.co <ngarciaorozco@outlook.com>
2021-05-28 15:44:48 -0600

    Navigation and Tab Bar Controllers

--- entrenate v2/entrenate v2/Controllers/Core Tabs/HomeViewController.swift
+++ entrenate v2/entrenate v2/Controllers/Core Tabs/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
---
+++ entrenate v2/entrenate v2/Controllers/Other/CompetenciaStartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompetenciaStartViewController.swift
+//  entrenate v2
+//
+//  Created by Nadia Garcia on 28/05/21.
+//
+
+import UIKit
+
+class CompetenciaStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

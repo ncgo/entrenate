cf791d5dfe1ab570e731fd3b16c8fc30279e5f68
Mauricio Ruffo <mauroruffo16@gmail.com>
2021-06-07 01:59:44 -0700

    Se agregó quitaTeclado y mueveTeclado

--- entrenate v2/entrenate v2/Controllers/Other/Onboarding/LoginViewController.swift
+++ entrenate v2/entrenate v2/Controllers/Other/Onboarding/LoginViewController.swift
@@ -111,7 +111,18 @@ class LoginViewController: UIViewController {
         usernameEmailField.delegate = self
         passwordField.delegate = self
         addSubviews()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
+        view.addGestureRecognizer(tap)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         // Do any additional setup after loading the view.
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     override func viewDidLayoutSubviews() {
@@ -232,4 +243,24 @@ extension LoginViewController: UITextFieldDelegate {
         }
         return true
     }
+    
+    @IBAction func quitaTeclado() {
+        view.endEditing(true)
+    }
+    
+    @objc func tecladoCambia(notification: Notification) {
+        print("Teclado aparece: \(notification.name.rawValue)")
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height
+        }
+        else {
+            view.frame.origin.y = 0
+        }
+        
+    }
+    
 }
--- entrenate v2/entrenate v2/Controllers/Other/Onboarding/RegisterViewController.swift
+++ entrenate v2/entrenate v2/Controllers/Other/Onboarding/RegisterViewController.swift
@@ -264,6 +264,36 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         self.pickerNivelEducativo.delegate = self
         self.pickerNivelEducativo.dataSource = self
         escuelaField.delegate = self
+        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
+        view.addGestureRecognizer(tap)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoCambia), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @IBAction func quitaTeclado() {
+        view.endEditing(true)
+    }
+    
+    @objc func tecladoCambia(notification: Notification) {
+        print("Teclado aparece: \(notification.name.rawValue)")
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            view.frame.origin.y = -keyboardRect.height
+        }
+        else {
+            view.frame.origin.y = 0
+        }
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -322,7 +352,10 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
 
 extension RegisterViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == usernameField {
+        if textField == nameField{
+            usernameField.becomeFirstResponder()
+        }
+        else if textField == usernameField {
             emailField.becomeFirstResponder()
         } else if textField == emailField {
             passwordField.becomeFirstResponder()

fa3cab90fbbbd7ddba8567cfdc5cdbbac2b7eb14
nadia.co <ngarciaorozco@outlook.com>
2021-06-06 19:41:43 -0600

    Profile Update (not done)

--- entrenate v2/entrenate v2/Views/Cells/ProfileTabsCollectionReusableView.swift
+++ entrenate v2/entrenate v2/Views/Cells/ProfileTabsCollectionReusableView.swift
@@ -7,16 +7,79 @@
 
 import UIKit
 
+protocol ProfileTabsCollectionReusableViewDelegate: AnyObject {
+    func didTapInfoButton()
+    func didTapStatsButton()
+    func didTapAchievementsButton()
+}
+
 class ProfileTabsCollectionReusableView: UICollectionReusableView {
     static let identifier = "ProfileTabsCollectionReusableView"
     
+    public weak var delegate: ProfileTabsCollectionReusableViewDelegate?
+    
+    private let infoButton: UIButton = {
+       let boton = UIButton()
+        boton.clipsToBounds = true
+        boton.tintColor = .systemGreen
+        boton.setBackgroundImage(UIImage(systemName: "info"), for: .normal)
+        return boton
+    }()
+    
+    private let statsButton: UIButton = {
+       let boton = UIButton()
+        boton.clipsToBounds = true
+        boton.tintColor = .secondarySystemBackground
+        boton.setBackgroundImage(UIImage(systemName: "house"), for: .normal)
+        return boton
+    }()
+    
+    private let achievementsButton: UIButton = {
+       let boton = UIButton()
+        boton.clipsToBounds = true
+        boton.tintColor = .secondarySystemBackground
+        boton.setBackgroundImage(UIImage(systemName: "person"), for: .normal)
+        return boton
+    }()
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor(red: 0.24, green: 0.57, blue: 0.90, alpha: 1.00)
+        backgroundColor = .systemGray3
+        addSubview(infoButton)
+        addSubview(statsButton)
+        addSubview(achievementsButton)
         
+        infoButton.addTarget(self, action: #selector(didTapInfoButton), for: .touchUpInside)
+        
+        statsButton.addTarget(self, action: #selector(didTapStatsButton), for: .touchUpInside)
+        
+        achievementsButton.addTarget(self, action: #selector(didTapAchievementsButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapInfoButton() {
+        delegate?.didTapInfoButton()
+    }
+    
+    @objc private func didTapStatsButton() {
+        delegate?.didTapStatsButton()
+    }
+    
+    @objc private func didTapAchievementsButton() {
+        delegate?.didTapAchievementsButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size = height/2
+        let widthBt = ((width/3)-size)/3
+        infoButton.frame = CGRect(x: widthBt, y: height/4, width: size, height: size)
+        statsButton.frame = CGRect(x: infoButton.right + 2*widthBt, y: height/4, width: size, height: size)
+        achievementsButton.frame = CGRect(x: statsButton.right + 2*widthBt, y: height/4, width: size, height: size)
     }
 }
